5bc2ea8d1325dc01b981d2fbbd5da18ed5e95fa8
Rowan Trollope <rowantrollope@gmail.com>
2025-03-18 23:20:15 -0700

    fixed button

--- app/vectorset/VectorSetNav.tsx
+++ app/vectorset/VectorSetNav.tsx
@@ -472,7 +472,7 @@ export default function VectorSetNav({
                                         </span>
                                         {info?.activeJobs > 0 && (
                                             <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-blue-600 rounded">
-                                                {info.activeJobs}
+                                                Importing...
                                             </span>
                                         )}
                                     </div>
